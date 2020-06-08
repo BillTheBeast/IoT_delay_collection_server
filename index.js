6a220a1d5b3e583488de9cc91b1659763be017fc
BillTheBeast <niko.mattila@pp.inet.fi>
2020-06-08 23:22:55 +0300

    Test

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 // Create a new instance of express
+const router = express.Router();
 const app = express()
 
 // Tell express to use the body-parser middleware and to not parse extended bodies
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // Route that receives a POST request to /sms
-app.post('/sms', function (req, res) {
+router.post('handle',(req, res) =>{
   const body = req.body.Body
   res.set('Content-Type', 'text/plain')
   res.send(`You sent: ${body} to Express`)
+  console.log(request.body);
 })
 
-// Tell our app to listen on port 3000
-app.listen(5000, function (err) {
-  if (err) {
-    throw err
-  }
-
-  console.log('Server started on port 3000')
+// Tell our app to listen on port 5000
+app.listen(5000,() =>{
+  console.log('Server started on port 5000')
 })
+
+app.use("/", router);
