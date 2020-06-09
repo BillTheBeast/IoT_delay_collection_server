41dd5829e13eb116a0e2d0679b676c25e32444c2
BillTheBeast <niko.mattila@pp.inet.fi>
2020-06-09 11:42:30 +0300

    Test

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const router = express.Router();
 const app = express()
 var body = ""
 var rtime = 0
+var msg = ""
+var arraystorage = []
 
 // Tell express to use the body-parser middleware and to not parse extended bodies
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -31,6 +33,8 @@ router.get('/',(req, res) =>{
 router.post('/',(req, res) =>{
 	rtime =Date.now()
   body = req.body.user
+  msg = {user:req.body.user,pass:req.body.password,rtime:rtime}
+  arraystorage.push(msg)
   res.set('Content-Type', 'text/plain')
   res.send(`You sent: ${body} to Express`)
   console.log("user: "+req.body.user+" pass: "+req.body.password+" Rtime: "+rtime);
