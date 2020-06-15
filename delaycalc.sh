d9807a1dfbcd9fed712517531e079c2762eb14e3
BillTheBeast <niko.mattila@pp.inet.fi>
2020-06-15 09:19:40 +0300

    Changed: Allowed shell scripts to take logfiles as parameters

--- delaycalc.sh
+++ delaycalc.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-echo "Provide the location of messagelog to calculate delays from"
-read MESSAGELOG
+if [$1]; then
+	MESSAGELOG = $1
+else
+	echo "Provide the location of messagelog to calculate delays from"
+	read MESSAGELOG
+fi
 curl -d "key=200&log=$MESSAGELOG" -X POST http://localhost:80/fnctn0
--- parselog.sh
+++ parselog.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-echo "Provide the location of receive_messagelog"
-read R_MESSAGELOG
-echo "Provide the location of sent_messagelog"
-read S_MESSAGELOG
+if [$1]; then
+	R_MESSAGELOG = $1
+else
+	echo "Provide the location of receive_messagelog"
+	read R_MESSAGELOG
+fi
+if [$2]; then
+	S_MESSAGELOG = $2
+else
+	echo "Provide the location of sent_messagelog"
+	read S_MESSAGELOG
+fi
 curl -d "key=100&rlog=$R_MESSAGELOG&slog=$S_MESSAGELOG" -X POST http://localhost:80/fnctn0
