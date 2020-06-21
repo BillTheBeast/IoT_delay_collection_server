4038b0b707d9af02b905a96b88518055c2851fdc
BillTheBeast <niko.mattila@pp.inet.fi>
2020-06-21 20:29:40 +0300

    Added: test bashfile to handle log parsing

--- parselog.bash
+++ parselog.bash
@@ -3,12 +3,14 @@ rfile="messagelog.txt"
 sfile="../lahetyslogi2.txt"
 while IFS= read -r rline
 do
-  echo "$rline" | awk -F"|" '{rnumh = $5} {rtime = $3} {mtime = $6}'
+	rnum=$(echo "$rline" | awk -F"|" '{printf $5}')
+	rtime=$(echo "$rline" | awk -F"|" '{printf $3}')
+	mtime=$(echo "$rline" | awk -F"|" '{printf $6}')
 	while IFS= read -r sline
 	do
-		echo "$sline" | awk -F" " '{snum = $4} {stime = $15} '
-		rnum = xxd -r "$rnumh" 
-		if [ "$rnum" == "$snum"]
+		snum=$(echo "$sline" | awk -F" " '{printf $4}')
+		stime=$(echo "$sline" | awk -F" " '{printf $15}')
+		if [ $(echo $rnum | xxd -r)=="$snum"]
 		then
 		echo "$snum|$rtime|$mtime|$stime"
 		fi
